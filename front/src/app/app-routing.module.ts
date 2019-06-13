597f0e11037140050b46a338fe6b662a82710ecd
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-13 11:46:16 +0100

    add routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BillsComponent} from './bills/bills.component';
+import {BillDetailsComponent} from './bill-details/bill-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bills',
+    component: BillsComponent,
+    // canActivate: [AuthGuard]
+  },
+  {
+    path: 'bill-detail',
+    component: BillDetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'bills',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
