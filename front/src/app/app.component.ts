214176041c5a8c1ff4266807b19d84a35267eba9
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-13 11:43:18 +0100

    update app

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthenticationService} from './_services/authentication.service';
+import {Router} from '@angular/router';
+import {User} from './_models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillsComponent } from './bills/bills.component';
+import { BillDetailsComponent } from './bill-details/bill-details.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { LoginComponent } from './login/login.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorInterceptor} from './_helpers/error.interceptor';
+import {JwtInterceptor} from './_helpers/jewt.interceptor';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent,
+    BillDetailsComponent,
+    AddUserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
