248f116a154e81d0400520ce453d7f29c9dee639
SkanderMez <36817030+SkanderMez@users.noreply.github.com>
2019-06-14 14:54:36 +0100

    s

--- back/src/main/java/com/accountingapi/repository/ProductRepository.java
+++ back/src/main/java/com/accountingapi/repository/ProductRepository.java
@@ -3,10 +3,8 @@ package com.accountingapi.repository;
 import com.accountingapi.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends CrudRepository<Product,String> {
 
-    List<Product> findProductsByBill_Bill_id(String id);
+   // List<Product> findProductsByBill_Bill_id(String id);
 
 }
