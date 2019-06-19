98e93c620d5d1744a5ceacfe5c41eb769b98f5a5
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-19 11:10:24 +0100

    add filter and order by filter

---
+++ front/src/app/_pipes/order-by.pipe.ts
@@ -0,0 +1,13 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
+
+@Pipe({ name: 'orderBy' })
+export class OrderByPipe implements PipeTransform {
+
+  transform(value: any[], column: string, order = ''): any[] {
+    if (!value || !column || column === '' || order === '') { return value; } // no array
+    if (value.length <= 1) { return value; } // array with only one item
+    return _.orderBy(value, [column], [order]);
+  }
+}
