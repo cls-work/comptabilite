4e5a918b693a129d2ff7e84b4a77124485609db9
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-19 11:09:36 +0100

    refractor jwt.interceptor

--- front/src/app/_helpers/jwt.interceptor.ts
+++ front/src/app/_helpers/jwt.interceptor.ts
@@ -11,10 +11,10 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
     const currentUser = this.authenticationService.currentUserValue;
-    if (currentUser && currentUser.token) {
+    if (currentUser && currentUser.accessToken) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
+          Authorization: 'Bearer ' + (JSON.parse(localStorage.getItem('currentUser'))).accessToken
         }
       });
     }
