5830526f3cb76d2cb3148163726cbc478101d18b
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-20 09:40:19 +0100

    Revert "edit user uri"
    This reverts commit 6a71d41fb93889fe77fe5f4c61048bd78a15b990.

--- back/src/main/java/com/accountingapi/security/controller/UserController.java
+++ back/src/main/java/com/accountingapi/security/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -21,12 +21,12 @@ public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping("")
+    @GetMapping("/users")
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/users/{id}")
     public User getUserById(@PathVariable(value = "id") Long id){
         return userRepository.getById(id);
     }
