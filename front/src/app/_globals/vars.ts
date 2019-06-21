308b56fda6fc7a7a1cac14a94937de3340cc1d4c
khlilturki97 <38277244+khlilturki97@users.noreply.github.com>
2019-06-21 11:06:29 +0100

    reset password impl

--- front/src/app/_globals/vars.ts
+++ front/src/app/_globals/vars.ts
@@ -5,4 +5,7 @@ export const AUTH = 'api/auth/';
 export const SIGN_IN = 'signin/';
 export const SIGN_UP = 'signup/';
 export const USERS = 'users/';
+export const FORGOT_PASSWORD = 'forgot-password/';
+export const VERIFY = 'verify/';
+export const NEW_PASSWORD = 'new-password/';
 
