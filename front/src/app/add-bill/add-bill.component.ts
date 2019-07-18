949edc7185f50e993ed0499c752997ab0aaf38bc
hamzadaoues <37672255+hamzadaoues@users.noreply.github.com>
2019-07-18 17:49:20 +0100

    add bil new component

--- front/src/app/add-bill/add-bill.component.ts
+++ front/src/app/add-bill/add-bill.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
+import {BillService} from '../_services/bill.service';
 
 @Component({
   selector: 'app-add-bill',
@@ -10,25 +11,49 @@ import {ActivatedRoute} from '@angular/router';
 export class AddBillComponent implements OnInit {
   labelPosition: boolean;
   cheque = true;
-  quotation_id: any;
+  quotation_id: number;
   quotationForm: FormGroup;
   checkform: FormGroup;
+  billform: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder, private route: ActivatedRoute) {
+    private formBuilder: FormBuilder, private route: ActivatedRoute ,
+    private  billService: BillService) {
   }
 
   ngOnInit() {
-    this.quotation_id = this.route.snapshot.params.id;
+    this.quotation_id = Number(this.route.snapshot.params.id);
     this.quotationForm = this.formBuilder.group({
       id: [this.quotation_id]
+    });
+
+    this.checkform = this.formBuilder.group({
+      amount: ['', Validators.required],
+      bankAccount: ['', Validators.required],
+      checkState: ['', Validators.required],
+      profilOf: ['', Validators.required],
+      reference: ['', Validators.required]
 
       // @ts-ignore
     });
+
   }
 
   editcheck(number: any) {
     this.cheque = number;
   }
 
+  addBill() {
+
+    this.billform = this.formBuilder.group({
+      checkPayment: [this.checkform.value],
+      quotation: [this.quotationForm.value]
+      // @ts-ignore
+    });
+
+    this.billService.postBill(this.billform.value).subscribe((res) => {
+      console.log(res) ;
+    }) ;
+    console.log(this.billform.value);
+  }
 }
